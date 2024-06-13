fd8d8ddb9cdc7fa340adbc46d683ee2b126ab780
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2024-06-13 07:59:54 +0000

    coversion

---
+++ basics_01/03_conversion_operation.js
@@ -0,0 +1,54 @@
+// let  score = 33 
+let  score = "33 abc" 
+// console.log(typeof score);
+
+// let valueInnumber = Number(score);
+// console.log(typeof valueInnumber);
+
+
+// console.log(valueInnumber)
+
+// "33"=> 33
+// "33 abc"=> NaN
+// true =>1; flase =>0
+// "aman" => false
+
+
+
+// let isLoggedIn = 1
+// let hi= Boolean(IsLoggedIn)
+// console.log(hi); 
+
+let someNumber = 33
+let hi= String(someNumber); // here the number is convert into string
+console.log(hi); // print '33'
+console.log(typeof hi); // it gives 'string'
+
+
+//*********************** operation **************************/
+
+// let  value=3
+// let negvalue =-value;
+//  console.log(negvalue);  // print '-3'
+
+
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2/2);
+//  console.log(2**2); // here means exponent symbol is = ** 
+//  console.log(2%2);
+
+
+ // adding two strings
+
+ let str1= "Aman";
+ let str2= " rao";
+
+ let str3= str1 + str2;
+ console.log(str3);
+
+let count= 99;
+count++;
+console.log(count);
