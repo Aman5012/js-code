57af46d86b5ca589cbf14da36011278e853bc111
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2024-06-18 15:26:02 +0000

    object

--- basics_02/03_object.js
+++ basics_02/03_object.js
@@ -8,7 +8,7 @@ const mySym = Symbol("key1")
 
 const JsUser = {
     name: "Aman",
-    // "full name": "Aman Rao" ;
+    "full name": 'Aman Rao',
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -16,13 +16,13 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["full name"])
-console.log(JsUser[mySym])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser) // freez the object values
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
@@ -30,8 +30,8 @@ JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    // console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`);
 }
 
-// console.log(JsUser.greeting());
-// console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
