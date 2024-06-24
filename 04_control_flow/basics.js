c268a6e859f526dfda8f096408328529e654de6c
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2024-06-24 09:29:07 +0000

    loops

---
+++ 04_control_flow/basics.js
@@ -0,0 +1,60 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, !==
+
+//  === it is used to check ther data types also
+// if(2=="2"){
+//     console.log( "correct");// print correct
+// }
+// if(2==="2"){
+//     console.log( "correct"); //  not print correct
+// }
+
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`); //  show error because power is not accessible outside of the scope
+
+
+
+// const balance = 1000
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
